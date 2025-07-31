235f9f6fd61194eb7a9816f28ef31fd0f8afaff3
dvankevich <dvankevich@gmail.com>
2025-07-31 20:37:06 +0300

    add and styling modal window

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[open-modal]"),
-    
-    closeModalBtn: document.querySelector("[close]"),
-    modal: document.querySelector("[modal]"),
-  };
-  refs.openModalBtn.addEventListener("click", toggleModal);
- 
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-  function toggleModal() {
-    refs.modal.classList.toggle("is-open");
-  }
+    const refs = {
+        openModalBtn: document.querySelector("[open-modal]"),
+        closeModalBtn: document.querySelector("[close]"),
+        modal: document.querySelector("[modal]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll"); // block scroll
+    }
 })();
